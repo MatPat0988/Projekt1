ffcf2aa09716b968ed665fc3e6b1f02a58b3d8cf
MatPat0988 <65392099+MatPat0988@users.noreply.github.com>
2020-06-18 12:06:52 +0200

    Na piąteczke

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,7 +1,16 @@
 module StudentsHelper
   def average(students)
-  len=students.collect{|s|
+  length=students.collect{|s|
     s.name.length
-}
-end
+
+    }
+  end
+  def show_mark (marks)
+    a=marks.collect{|m|m.mark}
+    if a.length>0
+       1.0* a.sum/a.length
+    else
+       "brak ocen"
+    end  
+  end
 end
